df7f92e32311e17d014681b192c5aec76a520b29
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-08-26 13:04:37 +0530

    Added: Function to simulate game play according to cases on snake and ladder

--- snakeAndLadder.js
+++ snakeAndLadder.js
@@ -1,5 +1,28 @@
 console.log("welcome to snake and ladder");
 
 var playerStartPostion = 0;
-let diceFaceValue = Math.floor(Math.random()*6 + 1);
-console.log(diceFaceValue);
+const NO_PLAY_CASE = 0;
+const LADDER_CASE = 1;
+const SNAKE_CASE = 2;
+
+let playerPositionCurrent = 0;
+function simulateGame() {
+
+    let diceFaceValue = Math.floor(Math.random() * 6 + 1);
+    
+    let playCases = Math.floor(Math.random()*3);
+
+    switch (playCases) {
+        case NO_PLAY_CASE:
+            playerPositionCurrent = playerPositionCurrent;
+            break;
+        case LADDER_CASE:
+            playerPositionCurrent = playerPositionCurrent + diceFaceValue;
+            break;    
+        case SNAKE_CASE:
+            playerPositionCurrent = playerPositionCurrent - diceFaceValue; 
+        default:
+            break;
+    }
+}
+simulateGame();
