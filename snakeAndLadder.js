e4bc6c3534b8a9fe2f571ba839b3cd533e260bd6
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-08-27 01:48:23 +0530

    Refactored: Code to play for two players by adding a switch player method

--- snakeAndLadder.js
+++ snakeAndLadder.js
@@ -8,9 +8,13 @@ const SNAKE_CASE = 2;
 let diceFaceValue;
 let diceRoll = 0;
 let playerPositionCurrent = 0;
+var lastplayer = 2;
+var playerOnePosition = START;
+var playerTwoPosition = START;
+var record = {};
 function simulateGame() {
     diceFaceValue = Math.floor(Math.random() * 6 + 1);
-    let playCases = Math.floor(Math.random()*3);
+    let playCases = Math.floor(Math.random() * 3);
     diceRoll++;
     switch (playCases) {
         case NO_PLAY_CASE:
@@ -18,28 +22,45 @@ function simulateGame() {
             break;
         case LADDER_CASE:
             playerPositionCurrent = playerPositionCurrent + diceFaceValue;
-            break;    
+            break;
         case SNAKE_CASE:
-            playerPositionCurrent = playerPositionCurrent - diceFaceValue; 
+            playerPositionCurrent = playerPositionCurrent - diceFaceValue;
         default:
             break;
     }
     if (playerPositionCurrent > WIN) {
         playerPositionCurrent = playerPositionCurrent - diceFaceValue;
     }
-    else if (playerPositionCurrent < START){
+    else if (playerPositionCurrent < START) {
         playerPositionCurrent = START;
     }
 
+    record['Player = ' + lastplayer] = playerPositionCurrent, diceRoll;
+
 }
 
-function playTillWinnerDeclared(){
-    while (true){
-        simulateGame();
-        if (playerPositionCurrent == WIN) {
-            console.log("Total Dice rolls to get a winner : " + diceRoll);
-            break;
+function playTillWinnerDeclared() {
+    while (playerPositionCurrent != WIN) {
+        switchPlayers();
     }
+    console.log("Player " + lastplayer + "won");
+    console.log("Player positon when game won are :");
+    console.log(record);
+    console.log("Total Dice rolls to declare winner is  " + diceRoll);
 }
+
+function switchPlayers() {
+    if (lastplayer == 1) {
+        lastplayer = 2;
+        playerPositionCurrent = playerTwoPosition;
+        simulateGame();
+        playerTwoPosition = playerPositionCurrent;
+    }
+    else {
+        lastplayer = 1;
+        playerPositionCurrent = playerOnePosition;
+        simulateGame();
+        playerOnePosition = playerPositionCurrent;
+    }
 }
 playTillWinnerDeclared();
